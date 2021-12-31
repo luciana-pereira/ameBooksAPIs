8ddf57793a2f0a9d6d1c19709df6790376ef7a3c
l.dos.santos.souza <l.dos.santos.souza@WINAP0RWVGYT816>
2021-12-31 20:34:40 -0300

    Add project files.

---
+++ AmeBooks/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmeBooks.Models
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+
+    }
+}
---
+++ AmeBooks/Models/ToDoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmeBooks.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> option) : base (option)
+        {
+
+        }
+
+        public DbSet<Product> todoProducts { get; set; }
+
+    }
+}
